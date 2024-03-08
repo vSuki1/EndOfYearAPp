0bb76badc686ea21abcff4b17fb371c6fa3b4d87
Disukhi <ahmeddisukhi@gmail.com>
2024-03-08 08:07:22 -0500

    Initial Commit

---
+++ EndOfYearAPp/EndOfYearAPpApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfYearAPpApp.swift
+//  EndOfYearAPp
+//
+//  Created by Disukhi Ahmed on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EndOfYearAPpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
